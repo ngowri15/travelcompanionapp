92dbd133ed8c526ffa429fffaffe37374a314778
Gowri Somasundaram <gowri@Gowris-MacBook-Pro.local>
2023-04-18 14:32:42 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
+import "@aws-amplify/ui-react/styles.css";
 import './App.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+function Profile({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Heading level={1}>Enter Additional Details</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
+{/*function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Travel Companion</h1>
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +50,13 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+  </a>
       </header>
     </div>
   );
-}
+}/*/}
 
-export default App;
+
+export default withAuthenticator(Profile);
+
+//export default App;
